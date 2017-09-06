7fbcbbf1b3026abc5734e716d5e7ec75c6986b5e
Chris Finlayson <chris@intellitect.com>
2017-09-06 14:28:50 -0700

    scaffolding

---
+++ seeddata.sql
@@ -0,0 +1,21 @@
+USE IpcStudent
+INSERT INTO dbo.Students
+        ( FirstName, LastName )
+VALUES  ( N'Jimi', -- FirstName - nvarchar(max)
+          N'Hendrix'  -- LastName - nvarchar(max)
+          )
+		  INSERT INTO dbo.Students
+        ( FirstName, LastName )
+VALUES  ( N'Eric', -- FirstName - nvarchar(max)
+          N'Clapton'  -- LastName - nvarchar(max)
+          )
+		  INSERT INTO dbo.Students
+        ( FirstName, LastName )
+VALUES  ( N'Anne', -- FirstName - nvarchar(max)
+          N'Wilson'  -- LastName - nvarchar(max)
+          )
+		  INSERT INTO dbo.Students
+        ( FirstName, LastName )
+VALUES  ( N'Joan', -- FirstName - nvarchar(max)
+          N'Jett'  -- LastName - nvarchar(max)
+          )
